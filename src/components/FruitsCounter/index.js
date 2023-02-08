41fbdca7eedf1ef1ff984871b9f4299b9e373f81
kiranraju115 <kiranraju115@gmail.com>
2023-02-08 03:17:11 +0000

    first commit

---
+++ src/components/FruitsCounter/index.js
@@ -0,0 +1,63 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class FruitsCounter extends Component {
+  state = {mango: 0, banana: 0}
+
+  onEatMango = () => {
+    this.setState(prevState => ({mango: prevState.mango + 1}))
+  }
+
+  onEatBanana = () => {
+    this.setState(prevState => ({banana: prevState.banana + 1}))
+  }
+
+  render() {
+    const {mango} = this.state
+    const {banana} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <h1 className="header">
+            Bob ate <span className="count">{mango}</span> mangoes
+            <span className="count"> {banana}</span> bananas
+          </h1>
+          <div className="image-container">
+            <div className="img-cont-1">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/mango-img.png "
+                alt="mango"
+                className="image"
+              />
+              <button
+                className="button"
+                type="button"
+                onClick={this.onEatMango}
+              >
+                Eat Mango
+              </button>
+            </div>
+            <div className="img-cont-1">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/banana-img.png"
+                alt="banana"
+                className="image"
+              />
+              <button
+                className="button"
+                type="button"
+                onClick={this.onEatBanana}
+              >
+                Eat Banana
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FruitsCounter
